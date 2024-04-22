9f5303a1b9991d9860e5ddbcfda7ff09cc87a702
sarthakportal2 <77792446+sarthakportal2@users.noreply.github.com>
2024-04-22 23:45:50 +0530

    Find_Triangular_Sum_of_An_Array.cpp

--- Find_Triangular_Sum_of_An_Array.cpp
+++ Find_Triangular_Sum_of_An_Array.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int triangularSum(vector<int>& nums) {
+        //T(C(N)==O(N)) and S(C(N)==O(1)) as it requires non memory space allocation iteratively
         int n=nums.size();//Nums'size declar
         while(n!=1){//Iterating through nums's size
             for (int i=0;i<n-1;i++){
