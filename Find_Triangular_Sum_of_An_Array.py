580197e67826c620c96d95b24411d06cd5f9d618
sarthakportal2 <77792446+sarthakportal2@users.noreply.github.com>
2024-04-22 23:44:53 +0530

    Delete Find_Triangular_Sum_of_An_Array.py

--- Find_Triangular_Sum_of_An_Array.py
+++ Find_Triangular_Sum_of_An_Array.py
@@ -1,9 +1,1 @@
-class Solution {
-public:
-    int triangularSum(vector<int>& nums) {
-        //T(C(N)==O(N)) and S(C(N)==O(1)) as it requires non memory space allocation iteratively
-        int n=nums.size();//Nums'size declar
-        while(n!=1){//Iterating through nums's size
-            for (int i=0;i<n-1;i++){
-                nums[i]=(nums[i]+nums[i+1])%10;//nums's modulo indx calc
-            }n--;}return nums[0];}};//decrementing nums's size and printing its 0th indx val
+
